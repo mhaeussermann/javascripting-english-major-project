5577a1ea0a3cc40109e31a86e37bc36025caad9f
Manuel H <mhaeussermann@users.noreply.github.com>
2018-12-11 16:49:51 +0100

    Solved Chapter 4 exercise

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ while(i < 4) {
       }
     i = i + 1;
 }
+
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  // 1. Calculate the percentage of the total 
+  // as a variable “tipAmount”
+  let tipAmount;
+  if (tipRate <= 100 && tipRate >= 1){
+    var tipRate
+    tipRate = tipRate * 0.01;
+  }
+  tipAmount = tipRate * total;
+  // 2. Change #response to tell us the tip
+  // amount.
+  $("#response").html("Your tip is $" + tipAmount);
+} 
+
+tipCalculator(50.00, 20);
