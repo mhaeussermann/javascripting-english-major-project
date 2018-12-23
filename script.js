be051f295e3fb2f3640d64cdb1784639208c6852
Manuel H <mhaeussermann@users.noreply.github.com>
2018-12-23 12:07:40 +0100

    Added bootstrap (Chapter 8)

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-// Exercise 1
-array_number = [2, 4, 6];
-let number_squared;
-number_squared = array_number.map(function(number){
-  return number*2;
-}).join(", ");
-$("#response").html(number_squared);
-
-// Exercise 2
-let leonardo, donatello, raphael, michelangelo, turtles, weapons;
-leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
-donatello = {name: "Donatello", color: "purple", weapon: "bo"};
-raphael = {name: "Raphael", color: "red", weapon: "sai"};
-michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
-turtles = [leonardo, donatello, raphael, michelangelo, mate];
-weapons = turtles.map(function(turtle){
-  return turtle.weapon;
-}).sort();
-weapons = weapons.slice(0, -1).join(', ') + ', and ' + weapons.slice(-1)
-// weapons is now "bo, katana, nunchaku, sai". Sorted, with commas.
-$("#response").html(weapons);
+let turtlesWithSplinter, reversedTurtlesWithoutSplinter;
+turtlesWithSplinter = ["Leonardo", "Donatello", "Raphael", "Michelangelo", "Splinter"];
+// Use .filter() instead of .pop().
+reversedTurtlesWithoutSplinter = turtlesWithSplinter.filter(function(turtle){
+  // What is the value of turtle?
+  console.log(turtle);
+  return turtle !== "Splinter";
+}).reverse().join(", ");
+$("#response").html(reversedTurtlesWithoutSplinter);
