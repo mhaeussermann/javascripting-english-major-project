8ac742e1984dec50032aed69bc9f83648a0be5cd
Manuel H <mhaeussermann@users.noreply.github.com>
2018-12-23 11:36:14 +0100

    Solutions to exercises 1 and 2 (Chapter 6)

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let userString, upperCaseMinusE, upperCasedString;
-userString = prompt("What do you want to UPPeRCASe?");
-upperCaseMinusE = function(string){
-  let result;
-  result = ""; 
-  for ( let i = 0; i < string.length ; i = i + 1 ) {
-    let letter;
-    letter = string[i];
-    if ( letter === "e" ) {
-      // Change here.
-      result += letter;
-    } else {
-      // And change here.
-      result += letter.toUpperCase();
-    }
-  }
-  return result;
-};
-upperCasedString = upperCaseMinusE(userString);
-$("#response").html(upperCasedString);
+// Exercise 1
+array_number = [2, 4, 6];
+let number_squared;
+number_squared = array_number.map(function(number){
+  return number*2;
+}).join(", ");
+$("#response").html(number_squared);
+
+// Exercise 2
+let leonardo, donatello, raphael, michelangelo, turtles, weapons;
+leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
+donatello = {name: "Donatello", color: "purple", weapon: "bo"};
+raphael = {name: "Raphael", color: "red", weapon: "sai"};
+michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
+turtles = [leonardo, donatello, raphael, michelangelo, mate];
+weapons = turtles.map(function(turtle){
+  return turtle.weapon;
+}).sort();
+weapons = weapons.slice(0, -1).join(', ') + ', and ' + weapons.slice(-1)
+// weapons is now "bo, katana, nunchaku, sai". Sorted, with commas.
+$("#response").html(weapons);
