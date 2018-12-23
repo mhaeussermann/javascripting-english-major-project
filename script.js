70becb5dbdc989e92a96cb6411d7e38525092bb3
Manuel H <mhaeussermann@users.noreply.github.com>
2018-12-23 10:46:36 +0100

    Chapter 6

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// Exercise 1: Function to return last item of array
-let arrayOfStrings, arrayOfNumbers, arrayMixed;
-arrayOfStrings = ["a", "b", "c"];
-arrayOfNumbers = [1, 2, 3];
-arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
-
-let checkarray
-checkarray = function(some_array){
-    $("#response").html(some_array[some_array.length - 1])
-};
-
-checkarray(arrayOfStrings)
-
-// Excercise 2: Integer check
-var numberquestion = parseInt(prompt("What's your number?"));
-if(Number.isInteger(numberquestion)){
-    $("#response").html("Your number is an integer");
-  } else {
-    $("#response").html("Your number is not an integer");
+let userString, upperCaseMinusE, upperCasedString;
+userString = prompt("What do you want to UPPeRCASe?");
+upperCaseMinusE = function(string){
+  let result;
+  result = ""; 
+  for ( let i = 0; i < string.length ; i = i + 1 ) {
+    let letter;
+    letter = string[i];
+    if ( letter === "e" ) {
+      // Change here.
+      result += letter;
+    } else {
+      // And change here.
+      result += letter.toUpperCase();
+    }
   }
+  return result;
+};
+upperCasedString = upperCaseMinusE(userString);
+$("#response").html(upperCasedString);
