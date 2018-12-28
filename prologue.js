2cdbc53c3b5e8b2824d0f164362827320b1479ea
Manuel H <mhaeussermann@users.noreply.github.com>
2018-12-29 00:36:40 +0100

    Progress in Chapter 9

--- prologue.js
+++ prologue.js
@@ -1,5 +1,5 @@
 $("#prologue").html("<p>The text of the Prologue will go here.</p>");
-$("#glosses").html("<p>The glosses will go here.</p>");
+//$("#glosses").html("<p>The glosses will go here.</p>");
 let line1, line1Text; // don’t need the intermediate step of line1TextArray
 line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill,",
         modern: "April,"}, {text: "with"}, {text: "his"}, {text: "shoures",
@@ -7,18 +7,21 @@ line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill,",
 // Create a blank string that opens two tags.
 line1Text = "<blockquote><p>";
 line1.forEach(function(word){
-    // Define a variable that will be the entirety of a single
-    // word-sized chunk of information.
     let wordString;
     wordString = word.text;
-    // Test to see if the .modern property exists.
     if (word.modern){
-      // If it does, surround wordString in an <a> tag.
-      wordString = "<a href='#'>" + wordString + "</a>";
+      // Add word.modern as a data attribute to the <a> tag.
+      wordString = "<a href='#' data-modern='" + word.modern + "'>" + wordString + "</a>";
     }
-    // Add wordString plus a space to the line1Text.
-    line1Text += wordString + " ";
+    line1Text = line1Text + wordString + " ";
   });
 // Break the line and close the two tags.
 line1Text += "<br />(line 2 would go here)</p></blockquote>";
 $("#prologue").html(line1Text);
+$("#prologue a").click(function(){
+    // Define the text and the word that was clicked.
+    let glossText, clickedWord;
+    clickedWord = $( this ).text();
+    glossText = "<h2>You clicked on the word: " + clickedWord + "</h2>";
+    $("#glosses").html(glossText);
+  });
