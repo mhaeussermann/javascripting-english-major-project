30b5e6afaf38365b80794ace157761a274df2cab
Manuel H <mhaeussermann@users.noreply.github.com>
2018-12-28 13:38:02 +0100

    Solved exercises in Chapter 8

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let turtlesWithSplinter, reversedTurtlesWithoutSplinter;
-turtlesWithSplinter = ["Leonardo", "Donatello", "Raphael", "Michelangelo", "Splinter"];
-// Use .filter() instead of .pop().
-reversedTurtlesWithoutSplinter = turtlesWithSplinter.filter(function(turtle){
-  // What is the value of turtle?
-  console.log(turtle);
-  return turtle !== "Splinter";
-}).reverse().join(", ");
-$("#response").html(reversedTurtlesWithoutSplinter);
+$("#end").addClass("lead");
